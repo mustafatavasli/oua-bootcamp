f5187c53ed049eb020de2615c9e6b9872dfbb23d
yigitozgul14 <myigit0214@gmail.com>
2024-07-14 12:41:46 +0300

    en son hale güncellendi

--- ouabootcamp/lib/pages/view/AlerjilerEkleScreen.dart
+++ ouabootcamp/lib/pages/view/AlerjilerEkleScreen.dart
@@ -17,132 +17,134 @@ class AlerjilerEkleScreen extends StatelessWidget {
           },
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Center(
-              child: Transform.translate(
-                offset: Offset(0, -20),
-                child: Image.asset('assets/images/Logo.png', height: 124, alignment: Alignment.topCenter),
-              ),
-            ),
-            SizedBox(height: 30),
-            Center(
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.8,
-                child: TextField(
-                  controller: _adController,
-                  decoration: InputDecoration(
-                    labelText: 'Alerji Adı',
-                    suffixIcon: Icon(Icons.person),
-                    labelStyle: TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.bold,
-                      fontSize: 14,
-                      color: Colors.black,
-                    ),
-                  ),
-                  style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Transform.translate(
+                  offset: Offset(0, -20),
+                  child: Image.asset('assets/images/Logo.png', height: 124, alignment: Alignment.topCenter),
                 ),
               ),
-            ),
-            SizedBox(height: 50),
-            Center(
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.8,
-                child: TextField(
-                  controller: _dereceController,
-                  decoration: InputDecoration(
-                    labelText: 'Alerji Derecesi',
-                    suffixIcon: Icon(Icons.person),
-                    labelStyle: TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                      color: Color.fromARGB(255, 0, 0, 0),
-                    ),
-                  ),
-                  style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontSize: 14,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 50),
-            Center(
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.8,
-                child: TextField(
-                  controller: _aciklamaController,
-                  decoration: InputDecoration(
-                    labelText: 'Alerji Açıklaması',
-                    suffixIcon: Icon(Icons.mail),
-                    labelStyle: TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
-                  style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 80),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Transform.translate(
-                  offset: Offset(-15, 0),
-                  child: SizedBox(
-                    width: 120,
-                    height: 48,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        backgroundColor: Color(0xFF353A48),
+              SizedBox(height: 30),
+              Center(
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  child: TextField(
+                    controller: _adController,
+                    decoration: InputDecoration(
+                      labelText: 'Alerji Adı',
+                      suffixIcon: Icon(Icons.person),
+                      labelStyle: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                        color: Colors.black,
                       ),
-                      onPressed: () {
-                        if (_adController.text.isNotEmpty &&
-                            _dereceController.text.isNotEmpty &&
-                            _aciklamaController.text.isNotEmpty) {
-                          Map<String, String> alerjiData = {
-                            'ad': _adController.text,
-                            'derece': _dereceController.text,
-                            'aciklama': _aciklamaController.text,
-                          };
-                          Navigator.pop(context, alerjiData);
-                        }
-                      },
-                      child: Text(
-                        'Alerji Ekle',
-                        style: TextStyle(
-                          fontFamily: 'Montserrat',
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
+                    ),
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 50),
+              Center(
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  child: TextField(
+                    controller: _dereceController,
+                    decoration: InputDecoration(
+                      labelText: 'Alerji Derecesi',
+                      suffixIcon: Icon(Icons.person),
+                      labelStyle: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 0, 0, 0),
+                      ),
+                    ),
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 14,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 50),
+              Center(
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  child: TextField(
+                    controller: _aciklamaController,
+                    decoration: InputDecoration(
+                      labelText: 'Alerji Açıklaması',
+                      suffixIcon: Icon(Icons.mail),
+                      labelStyle: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 80),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Transform.translate(
+                    offset: Offset(-15, 0),
+                    child: SizedBox(
+                      width: 120,
+                      height: 48,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Color(0xFF353A48),
+                        ),
+                        onPressed: () {
+                          if (_adController.text.isNotEmpty &&
+                              _dereceController.text.isNotEmpty &&
+                              _aciklamaController.text.isNotEmpty) {
+                            Map<String, String> alerjiData = {
+                              'ad': _adController.text,
+                              'derece': _dereceController.text,
+                              'aciklama': _aciklamaController.text,
+                            };
+                            Navigator.pop(context, alerjiData);
+                          }
+                        },
+                        child: Text(
+                          'Alerji Ekle',
+                          style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- ouabootcamp/lib/pages/view/YakinlarimEkleScreen.dart
+++ ouabootcamp/lib/pages/view/YakinlarimEkleScreen.dart
@@ -18,160 +18,162 @@ class YakinlarimEkleScreen extends StatelessWidget {
           },
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Center(
-              child: Transform.translate(
-                offset: Offset(0, -20),
-                child: Image.asset('assets/images/Logo.png', height: 124, alignment: Alignment.topCenter),
-              ),
-            ),
-            SizedBox(height: 10),
-            Center(
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.8,
-                child: TextField(
-                  controller: _adController,
-                  decoration: InputDecoration(
-                    labelText: 'Yakının Adı',
-                    suffixIcon: Icon(Icons.person),
-                    labelStyle: TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.bold,
-                      fontSize: 14,
-                      color: Colors.black,
-                    ),
-                  ),
-                  style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Transform.translate(
+                  offset: Offset(0, -20),
+                  child: Image.asset('assets/images/Logo.png', height: 124, alignment: Alignment.topCenter),
                 ),
               ),
-            ),
-            SizedBox(height: 50),
-            Center(
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.8,
-                child: TextField(
-                  controller: _soyadController,
-                  decoration: InputDecoration(
-                    labelText: 'Yakının Soyadı',
-                    suffixIcon: Icon(Icons.person),
-                    labelStyle: TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
-                  style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 50),
-            Center(
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.8,
-                child: TextField(
-                  controller: _telefonController,
-                  decoration: InputDecoration(
-                    labelText: 'Telefon Numarası',
-                    suffixIcon: Icon(Icons.phone),
-                    labelStyle: TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
-                  style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 50),
-            Center(
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.8,
-                child: TextField(
-                  controller: _yakinlikController,
-                  decoration: InputDecoration(
-                    labelText: 'Yakınlık Derecesi',
-                    suffixIcon: Icon(Icons.family_restroom),
-                    labelStyle: TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
-                  style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 40),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Transform.translate(
-                  offset: Offset(-15, 0),
-                  child: SizedBox(
-                    width: 120,
-                    height: 48,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        backgroundColor: Color(0xFF353A48),
+              SizedBox(height: 10),
+              Center(
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  child: TextField(
+                    controller: _adController,
+                    decoration: InputDecoration(
+                      labelText: 'Yakının Adı',
+                      suffixIcon: Icon(Icons.person),
+                      labelStyle: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                        color: Colors.black,
                       ),
-                      onPressed: () {
-                        if (_adController.text.isNotEmpty &&
-                            _soyadController.text.isNotEmpty &&
-                            _telefonController.text.isNotEmpty &&
-                            _yakinlikController.text.isNotEmpty) {
-                          Map<String, String> yakinData = {
-                            'ad': _adController.text,
-                            'soyad': _soyadController.text,
-                            'telefon': _telefonController.text,
-                            'yakinlik': _yakinlikController.text,
-                          };
-                          Navigator.pop(context, yakinData);
-                        }
-                      },
-                      child: Text(
-                        'Yakın Ekle',
-                        style: TextStyle(
-                          fontFamily: 'Montserrat',
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
+                    ),
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 50),
+              Center(
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  child: TextField(
+                    controller: _soyadController,
+                    decoration: InputDecoration(
+                      labelText: 'Yakının Soyadı',
+                      suffixIcon: Icon(Icons.person),
+                      labelStyle: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 50),
+              Center(
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  child: TextField(
+                    controller: _telefonController,
+                    decoration: InputDecoration(
+                      labelText: 'Telefon Numarası',
+                      suffixIcon: Icon(Icons.phone),
+                      labelStyle: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 50),
+              Center(
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  child: TextField(
+                    controller: _yakinlikController,
+                    decoration: InputDecoration(
+                      labelText: 'Yakınlık Derecesi',
+                      suffixIcon: Icon(Icons.family_restroom),
+                      labelStyle: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 40),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Transform.translate(
+                    offset: Offset(-15, 0),
+                    child: SizedBox(
+                      width: 120,
+                      height: 48,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Color(0xFF353A48),
+                        ),
+                        onPressed: () {
+                          if (_adController.text.isNotEmpty &&
+                              _soyadController.text.isNotEmpty &&
+                              _telefonController.text.isNotEmpty &&
+                              _yakinlikController.text.isNotEmpty) {
+                            Map<String, String> yakinData = {
+                              'ad': _adController.text,
+                              'soyad': _soyadController.text,
+                              'telefon': _telefonController.text,
+                              'yakinlik': _yakinlikController.text,
+                            };
+                            Navigator.pop(context, yakinData);
+                          }
+                        },
+                        child: Text(
+                          'Yakın Ekle',
+                          style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
